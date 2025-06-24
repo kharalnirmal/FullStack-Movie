805064ade5231caad3a4b967c62b0dfeb769f1ae
Nirmal-Kharal <nirmalkharal40@gmail.com>
2025-06-24 09:01:00 +0545

    added / cursor animation

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import MoviesCards from './components/MoviesCards';
 import { useDebounce } from 'react-use';
 import { getTrendingMovies, updateSearchCount } from './appWrite';
 
+
 const App = () => {
   const API_BASE_URL = 'https://api.themoviedb.org/3';
   const API_KEY = import.meta.env.VITE_TMDB_API_KEY; // Must be v4 Bearer token
